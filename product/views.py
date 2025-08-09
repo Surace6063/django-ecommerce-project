50e4344d7ece1ee379ad050a80c15d35dcdc6dae
Suresh Thapa <sureshthapa6063@gmail.com>
2025-08-09 13:06:40 +0545

    product model created and retrived

--- product/views.py
+++ product/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
 def products(request):
-    return HttpResponse('Product Lists')
+    product_lists = Product.objects.all().order_by('-id')
+    items = {
+        "products":product_lists
+        }
+
+    return render(request,'product/product_lists.html',items)
+
+
+def product_details(request,product_id):
+    product = Product.objects.get(id = product_id)
+
+    data = {
+        'product':product
+    }
+    return render(request,'product/product_details.html',data)
